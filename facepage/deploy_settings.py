92f0ec78b8cae151a84b6195ee5ee5b5236959f5
andrew <androw.emad@yahoo.com>
2022-07-13 21:34:30 +0200

    fixed the wrong path for css file in register page

--- facepage/deploy_settings.py
+++ facepage/deploy_settings.py
@@ -8,7 +8,6 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATICFILES_STORAGE = 'facepage.storage.WhiteNoiseStaticFilesStorage'
 
 ALLOWED_HOSTS = [
     'facepage.herokuapp.com', '127.0.0.1'
